5b0e7b64417730c5ba8789c7504857a12e64e1aa
Betram Manganda <betrammanganda@hotmail.com>
2023-03-10 23:48:14 +0000

    Adding more list items

--- src/components/list_todo/ListTodo.jsx
+++ src/components/list_todo/ListTodo.jsx
@@ -8,6 +8,16 @@ export default function ListTodo() {
         <div className="ListTodo__item">Buy New Laptop</div>
         <div className="ListTodo__delete">Del</div>
       </div>
+
+      <div className="ListTodo__row">
+        <div className="ListTodo__item">Make Codehance Course</div>
+        <div className="ListTodo__delete">Del</div>
+      </div>
+
+      <div className="ListTodo__row">
+        <div className="ListTodo__item">Complete Today's Task</div>
+        <div className="ListTodo__delete">Del</div>
+      </div>
     </div>
   );
 }
